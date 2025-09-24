c48e23840f50b6814ca5c59639db36155963b81c
AlkaidSTART <2595006848@qq.com>
2025-09-24 21:55:30 +0800

    完成插槽1基本狂阶开发

---
+++ src/components/notion.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="head">
+        <div class="head-box">Features</div>
+        <div class="head-box">Get started</div>
+    </div>
+    <div class="items">
+        <div class="item">
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+        </div>
+         <div class="item">
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+        </div>
+          <div class="item">
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+            <div class="item-box">
+                <div class="text"></div>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <footer class="footer-box">
+        <div class="footer-left">
+            <span></span>
+            <button></button>
+        </div>
+        <div class="footer-right">
+            <img src="" alt="">
+        </div>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+<style scoped></style>
